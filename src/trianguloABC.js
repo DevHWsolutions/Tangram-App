278cc64352371bfbc8b5a153cf3e1a570399aead
DevHWsolutions <hans.gonzalez@hwsolutions.com.gt>
2025-05-09 11:41:56 -0600

    Feature: Inicio de repositorio tangramapp

---
+++ src/trianguloABC.js
@@ -0,0 +1,72 @@
+export const triangulo = [
+    {
+      id: "p1",
+      nombre: "Triángulo Grande 1",
+      points: [0, 0, 200, 0, 0, 200],
+      x: 230,
+      y: 250,
+      fill: "skyblue",
+      rotation: 45,
+      draggable: true
+    },
+    {
+      id: "p2",
+      nombre: "Triángulo Grande 2",
+      points: [0, 0, 200, 0, 0, 200],
+      x: 528,
+      y: 250,
+      fill: "orange",
+      rotation: 45,
+      draggable: true
+    },
+    {
+      id: "p3",
+      nombre: "Triángulo Mediano",
+      points: [0, 0, 140, 0, 0, 140],
+      x: 382,
+      y: 250,
+      fill: "magenta",
+      rotation: 0,
+      draggable: true
+    },
+    {
+      id: "p4",
+      nombre: "Triángulo Pequeño 1",
+      points: [0, 0, 100, 0, 0, 100],
+      x: 307,
+      y: 321,
+      fill: "lime",
+      rotation: 315,
+      draggable: true
+    },
+    {
+      id: "p5",
+      nombre: "Triángulo Pequeño 2",
+      points: [0, 0, 100, 0, 0, 100],
+      x: 380,
+      y: 100,
+      fill: "yellow",
+      rotation: 45,
+      draggable: true
+    },
+    {
+      id: "p6",
+      nombre: "Cuadrado",
+      points: [0, 0, 100, 0, 100, 100, 0, 100],
+      x: 305,
+      y: 176,
+      fill: "blue",
+      rotation: 45,
+      draggable: true
+    },
+    {
+      id: "p7",
+      nombre: "Paralelogramo",
+      points: [-85, -10, 55, -10, 125, 60, -20, 60],
+      x: 435,
+      y: 235,
+      fill: "teal",
+      rotation: 180,
+      draggable: true
+    }
+  ];
