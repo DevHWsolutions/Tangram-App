f5cc4e36b251c50d8c2b530324e1a608410a711a
DevHWsolutions <hans.gonzalez@hwsolutions.com.gt>
2025-05-23 09:44:14 -0600

    Fixed: Se ha instalado legacy para probrar que funcione con safari antiguio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    legacy({
+      targets: ["defaults", "not IE 11"], // puedes ajustar según tu necesidad
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"], // si usas async/await
+      modernPolyfills: true,
+    }),
+  ],
+});
